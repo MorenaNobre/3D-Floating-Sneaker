764e891fba5da19b94a78860f4f7ae1b9b1ab6ae
MorenaNobre <morenagnobre@gmail.com>
2021-04-03 08:56:41 -0300

    floating-sneaker

--- README.md
+++ README.md
@@ -1,70 +1,60 @@
-# Getting Started with Create React App
+<h1 align="center">Projeto ThreeJS - Tênis Flutuante 3D</h1>
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+[![Linkedin Badge](https://img.shields.io/badge/-MorenaNobre-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/morenanobre/)](https://www.linkedin.com/in/morenanobre/)
+[![Gmail Badge](https://img.shields.io/badge/-morenagnobre@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:morenagnobre@gmail.com)](mailto:morenagnobre@gmail.com)
 
-## Available Scripts
+<blockquote align="center">“A chave para transformação é a continuidade. Continue a codar!!!”</blockquote>
 
-In the project directory, you can run:
+<br>
 
-### `yarn start`
+<p align="center">Deploy <a href="https://blank-sneaker.netlify.app/" target="_blank">aqui!</a></p>
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+<br>
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+<hr>
 
-### `yarn test`
+<br>
+<br>
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Sumário
 
-### `yarn build`
+- [Apresentação](#-apresentação)
+- [Sobre o projeto](#-sobre-o-projeto)
+- [Tecnologias utilizadas](#-tecnologias-utilizadas)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+<br>
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+## Apresentação
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+<br>
+<div align="center">
+  <img alt="floating-sneaker" src="floating-sneaker.gif" width="100%">
+</div>
+<br>
 
-### `yarn eject`
+## Sobre o Projeto
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+<br>
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+<p>A cada dia a tecnologia avança mais e não é diferente com o desenvolvimento web. Tem surgido cada vez mais sites e landing pages interativos, em alguns deles pode-se perceber a utilização de interação 3D, como é o caso desse site da <a href="https://adidaschile20.com/" target="_blank"> Adidas</a>.</p>
+<p>Com isso tenho cada vez mais interesse em aprender novas tecnologias como o THREEJS.</p>
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+<br>
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+## Tecnologias Utilizadas
 
-## Learn More
+<br>
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+<code><img height="32" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" alt="React"/></code>
+<code><img height="32" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" alt="Javascript"/></code>
+<code><img height="32" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" alt="CSS"/></code>
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+<br>
+<br>
+<hr>
+<br>
+<br>
 
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+<h3 align="center">
+    <img alt="mayTheCodeBeWithYou" src="mayCode.png" height="80px" />
+</h3>
--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useRef, useState, useEffect } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { ContactShadows, Environment, useGLTF, OrbitControls } from "drei";
+import { HexColorPicker } from "react-colorful";
+import { proxy, useProxy } from "valtio";
 
-function App() {
+const state = proxy({
+  current: null,
+  items: {
+    laces: "#ffffff",
+    mesh: "#ffffff",
+    caps: "#ffffff",
+    inner: "#ffffff",
+    sole: "#ffffff",
+    stripes: "#ffffff",
+    band: "#ffffff",
+    patch: "#ffffff",
+  },
+});
+
+function Shoe(props) {
+  const group = useRef();
+  const snap = useProxy(state);
+  const { nodes, materials } = useGLTF("/shoe-draco.glb");
+
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    group.current.rotation.z = -0.2 - (1 + Math.sin(t / 1.5)) / 20
+    group.current.rotation.x = Math.cos(t / 4) / 8
+    group.current.rotation.y = Math.sin(t / 4) / 8
+    group.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+  })
+
+  const [hovered, setHovered] = useState(null);
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`;
+    document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(
+      hovered ? cursor : auto
+    )}'), auto`;
+  }, [hovered]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <group
+      ref={group}
+      dispose={null}
+      onPointerOver={(e) => {
+        e.stopPropagation();
+        setHovered(e.object.material.name);
+      }}
+      onPointerOut={(e) => {
+        e.intersections.length === 0 && setHovered(null);
+      }}
+      onPointerDown={(e) => {
+        e.stopPropagation();
+        state.current = e.object.material.name;
+      }}
+      onPointerMissed={() => {
+        state.current = null;
+      }}
+    >
+      <mesh
+        material-color={snap.items.laces}
+        material={materials.laces}
+        geometry={nodes.shoe.geometry}
+      />
+      <mesh
+        material-color={snap.items.mesh}
+        material={materials.mesh}
+        geometry={nodes.shoe_1.geometry}
+      />
+      <mesh
+        material-color={snap.items.caps}
+        material={materials.caps}
+        geometry={nodes.shoe_2.geometry}
+      />
+      <mesh
+        material-color={snap.items.inner}
+        material={materials.inner}
+        geometry={nodes.shoe_3.geometry}
+      />
+      <mesh
+        material-color={snap.items.sole}
+        material={materials.sole}
+        geometry={nodes.shoe_4.geometry}
+      />
+      <mesh
+        material-color={snap.items.stripes}
+        material={materials.stripes}
+        geometry={nodes.shoe_5.geometry}
+      />
+      <mesh
+        material-color={snap.items.band}
+        material={materials.band}
+        geometry={nodes.shoe_6.geometry}
+      />
+      <mesh
+        material-color={snap.items.patch}
+        material={materials.patch}
+        geometry={nodes.shoe_7.geometry}
+      />
+    </group>
+  );
+}
+
+function Picker() {
+  const snap = useProxy(state);
+  return (
+    <div style={{ display: snap.current ? "block" : "none" }}>
+      <HexColorPicker
+        className="picker"
+        color={snap.items[snap.current]}
+        onChange={(color) => (state.items[snap.current] = color)}
+      />
+      <h1>{snap.current}</h1>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Canvas
+        concurrent
+        pixelRatio={[1, 1.5]}
+        camera={{ position: [0, 0, 2.35] }}
+      >
+        <ambientLight intensity={0.3} />
+        <spotLight
+          intensity={0.3}
+          angle={0.1}
+          penumbra={1}
+          position={[5, 25, 20]}
+        />
+        <Suspense fallback={null}>
+          <Shoe />
+          <Environment files="hdr.hdr" />
+          <ContactShadows
+            rotation-x={Math.PI / 2}
+            position={[0, -0.8, 0]}
+            opacity={0.25}
+            width={10}
+            height={10}
+            blur={2}
+            far={1}
+          />
+        </Suspense>
+        <OrbitControls
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+          enableZoom={false}
+          enablePan={false}
+        />
+      </Canvas>
+      <Picker />
+    </>
+  );
+}
